324bdca1bb6fcce052fe76d16971227fa90b1bca
HookSuite <107318325+HookSuite@users.noreply.github.com>
2022-10-04 06:23:50 +0000

    removed log

--- src/coinflip.c
+++ src/coinflip.c
@@ -28,4 +28,4 @@ void generateRandom(int amount)
         }
 
     FILE* file = fopen("results.txt", "w");
-    fprintf(file, "%d heads and %d tails of total %d flips.\n", head
+    fprintf(file, "%d heads and %d tails of total %d flips.\n", heads
