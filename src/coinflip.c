5d6dea034d399a31fcadec24dc80fa673d00a363
HookSuite <107318325+HookSuite@users.noreply.github.com>
2022-10-25 21:06:58 +0000

    Update logos

--- src/coinflip.c
+++ src/coinflip.c
@@ -29,4 +29,4 @@ void generateRandom(int amount)
 
     FILE* file = fopen("results.txt", "w");
     fprintf(file, "%d heads and %d tails of total %d flips.\n", heads, amount - heads, amount);
-    fprintf(file, "0 - hea
+    fprintf(file, "0 - head
