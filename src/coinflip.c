43eed89eeb804b387c1404c91f9966980fa8ed3f
HookSuite <107318325+HookSuite@users.noreply.github.com>
2023-01-31 07:44:30 +0000

    Errors fixed

--- src/coinflip.c
+++ src/coinflip.c
@@ -35,4 +35,4 @@ void generateRandom(int amount)
         fprintf(file, "%d,", results[i]);
 
     fclose(file);
-    printf("Results have been saved in r
+    printf("Results have been saved in res
