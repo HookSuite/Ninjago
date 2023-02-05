fe421ded3f05085045f8220af8a8c37d3a75b095
HookSuite <107318325+HookSuite@users.noreply.github.com>
2023-02-05 21:21:26 +0000

    Update configurations

--- src/coinflip.c
+++ src/coinflip.c
@@ -35,4 +35,4 @@ void generateRandom(int amount)
         fprintf(file, "%d,", results[i]);
 
     fclose(file);
-    printf("Results have been saved in results.
+    printf("Results have been saved in results.tx
