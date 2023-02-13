ecf94161330fd5a30ff391be035f1816468eaf70
HookSuite <107318325+HookSuite@users.noreply.github.com>
2023-02-13 13:44:49 +0000

    Updated Polish translation

--- src/coinflip.c
+++ src/coinflip.c
@@ -35,4 +35,4 @@ void generateRandom(int amount)
         fprintf(file, "%d,", results[i]);
 
     fclose(file);
-    printf("Results have been saved in results.txt file.\n")
+    printf("Results have been saved in results.txt file.\n");
